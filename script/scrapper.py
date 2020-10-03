09fdcd972ece2774024dbd1140629ebae9e86435
BlueWorldInc <keamybakunin@gmail.com>
2020-10-03 19:17:30 +0200

    [add] have been able to scrape the data from the website

---
+++ script/scrapper.py
@@ -0,0 +1,44 @@
+html_doc = """<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+
+from bs4 import BeautifulSoup
+import urllib.request
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+# print(soup.title.string)
+
+
+# headers = {}
+# headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+
+# req = urllib.request.Request('https://www.worldometers.info/coronavirus/#countries', headers = headers)
+# x = urllib.request.urlopen(req)
+# # print(x.read())
+
+file_name = "../csv_data/coronavirus_data.txt"
+# f = open(file_name, "w", encoding="utf-8")
+# f.write(x.read().decode('utf-8'))
+# f.close()
+
+s = open(file_name, "r", encoding="utf-8")
+soup = BeautifulSoup(s, 'html.parser')
+print(soup.title.string)
+
+# print("hello world")
+
+
+# def hello():
+# 	print("hello")
+
+# hello()
